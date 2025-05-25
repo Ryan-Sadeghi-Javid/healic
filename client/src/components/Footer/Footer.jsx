9679cff551ea9bc0461cbb52e87126855bebbb41
Ryan-Sadeghi-Javid <javidryan46@gmail.com>
2025-05-25 03:52:52 -0400

    fix: frontend footer changes

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -10,12 +10,11 @@ export default function Footer() {
         <div className="row">
           <div className="col l6 s12">
             <h5 className="white-text">Made with ❤️</h5>
-            <p className="grey-text text-lighten-4">Created for the 2022 NextStep Hacks hackathon.</p>
           </div>
           <div className="col l4 offset-l2 s12">
             <h5 className="white-text">Links</h5>
             <ul>
-              <li><a className="grey-text text-lighten-3" href="https://github.com/MysticalX99/nextstep-hacks" target='_blank' rel="noreferrer"><img src={Github} alt='' style={{width: 24, height: 24, marginRight: 10, marginBottom: 4}}></img> Source Code</a></li>
+              <li><a className="grey-text text-lighten-3" href="https://github.com/Ryan-Sadeghi-Javid/healic" target='_blank' rel="noreferrer"><img src={Github} alt='' style={{width: 24, height: 24, marginRight: 10, marginBottom: 4}}></img> Source Code</a></li>
               <li><a className="grey-text text-lighten-3" href="https://devpost.com/software/healic" target='_blank' rel="noreferrer"><img src={Devpost} alt='' style={{width: 32, height: 32, marginRight: 5, marginLeft: -3}}></img> Devpost</a></li>
             </ul>
           </div>
@@ -23,7 +22,7 @@ export default function Footer() {
       </div>
       <div className="footer-copyright">
         <div className="container" style={{textAlign: 'center'}}>
-        © 2022 Danyal, Ryan, Tim
+        © 2025 Ryan Sadeghi-Javid
         </div>
       </div>
     </footer>
